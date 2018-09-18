176487070443b72d68afa2e2bf3d00e671ea8ce0
aaa <aaa@aaadembp.setup\.net>
2018-09-18 20:07:48 +0800

    Initial Commit

---
+++ MarkSix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MarkSix
+//
+//  Created by aaa on 18/9/2018.
+//  Copyright © 2018年 Kam Lung Poon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
